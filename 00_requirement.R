5deb3818bb8f341fe09c6248eb1b0496bbd84fe2
versaell <didihe1114@gmail.com>
2025-11-07 11:40:34 -0800

    more advanced requirements file

--- 00_requirement.R
+++ 00_requirement.R
@@ -1,4 +1,10 @@
-library(stringr)
-library(tidyverse)
-library(magrittr)
-library(stopwords)
+packages <- c("stringr", "tidyverse", "readxl", "magrittr", "dplyr", "ggplot2", "knitr", "stopwords")
+
+for (pkg in packages) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg)
+    library(pkg, character.only = TRUE)
+  } else {
+    library(pkg, character.only = TRUE)
+  }
+}
